b3fd560687660df721e06ca9d333f3d4fe0e7ee1
fxrifty <logan.stevens@gmx.com>
2024-05-02 12:49:44 -0700

    complete

--- task1.py
+++ task1.py
@@ -1,6 +1,18 @@
 def A():
+ print('Hello')
 
 def B():
-
+    print('How are you?')
 
 def C():
+    print('Hi')
+
+x=input('Enter a letter from A-C\n')
+if x == 'A'or 'a': 
+ A()
+else:
+    print()
+if x == 'B'or 'b':
+ B()
+if x == 'C'or 'c':
+ C()
--- task2.py
+++ task2.py
@@ -8,3 +8,22 @@ displays instructions and how to play
 game()
 plays the games
 """
+import random
+def title():
+ print('Hello! In This Game You Have 1 Chance To Guess A Number Between 1 And 10. Good Luck!')
+x=input("Enter Your Guess Here")
+
+
+
+title()
+name = input('Enter a Username ')
+print('Welcome',name)
+numbers=('1','2','3','4''5','6','7','8''9','10')
+answer=random.choice(numbers)
+x=input("Enter Your Guess Here\n")
+if x == answer:
+ print("Congradulations {name}! {x} Was The Answer")
+else:
+ print("Nice Try") 
+
+ 
